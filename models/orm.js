6e5be6edd8f7678954212729054a85b3e50f763f
Sitthiph <47881002+sitthiph@users.noreply.github.com>
2019-10-07 02:20:37 -0400

    pending_table added

--- models/orm.js
+++ models/orm.js
@@ -13,7 +13,7 @@ const connection = require("./connection.js");
    *  {firstName, lastName, gender, age, pictures, description}
    *  @param userId - account's userId
    */
-  function displaySwipe(userId) {
+  function getUnmatch(userId) {
     let queryString = "SELECT * FROM ?? WHERE ?? = ?";
     connection.query(queryString, ["user_table", "user_id", userId], function(err, result) {
       if (err) throw err;
@@ -21,13 +21,18 @@ const connection = require("./connection.js");
       // TODO: THIS DATA NEED TESTING; IDK WHAT IT'S RETURNING;
     });
   }
-
+  
+  function signIn(userId) {
+    let queryString = "SELECT * FROM ?? WHERE ?? = ? ";
+  }
+  
 /**
  *
  * @param userId1 - The Person's account id;
  * @param userId2 - The Matched's partner id;
+ * REMOVE DUE TO BEING ICE BOXED.
  */
-function conversationPage(userId1, userId2) {
+/*function conversationPage(userId1, userId2) {
     let queryString = "SELECT * FROM ?? WHERE ?? = ? AND ?? = ?";
     console.log(queryString);
     connection.query(queryString, ["chat_Table", "user_id_1", userId1, "user_id_2", userId2], function(err, result) {
@@ -35,8 +40,8 @@ function conversationPage(userId1, userId2) {
       console.log(result);
       return result;
     });
-  }
-  function matchedPage(userId) {
+  }*/
+  function checkMatch(userId) {
     let queryString = "SELECT * FROM ?? WHERE ?? = ?";
   }
 connection.query(queryString,
